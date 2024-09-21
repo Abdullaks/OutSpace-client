8726d09f0fa3cac31ccf56d35e995c0477b5933d
Abdullaks <abdullaks1999@gmail.com>
2024-09-21 21:49:59 +0530

    fixes

--- src/components/createPost/index.js
+++ src/components/createPost/index.js
@@ -28,7 +28,12 @@ export default function CreatePost({ user, setPostPopup }) {
           <LiveVideo color="#f3425f" />
           Live Video
         </div>
-        <div className="createPost_icon hover1">
+        <div
+          className="createPost_icon hover1"
+          onClick={() => {
+            setPostPopup(true);
+          }}
+        >
           <Photo color="#4bbf67" />
           Photo/Video
         </div>
